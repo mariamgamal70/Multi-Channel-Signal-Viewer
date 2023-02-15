b2cd786be92885009e4f02aa1ee158004d06caf0
mariamgamal70 <mariamgamal70@gmail.com>
2023-02-16 00:50:54 +0200

    tried to create an initial shape

---
+++ project.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect, url_for, request
+# Handle the HTTP request
+app = Flask(__name__)
+@app.route('/',methods=["POST","GET"])
+def getpath():
+    if request.method == 'POST':
+        filePath = request.form['filePath']
+    return render_template("/index.html")    
+if __name__ == '__main__':
+    app.run(debug=True)

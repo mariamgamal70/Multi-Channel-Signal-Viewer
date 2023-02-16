5452e9c5826b53958c2ddd0fcdd10b06e19bd78b
mariamgamal70 <mariamgamal70@gmail.com>
2023-02-16 15:49:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/',methods=["POST","GET"])
 def getpath():
     if request.method == 'POST':
-        file = request.files.get('uploadedsignal')
+        file = request.files.get('uploadedsignal') #uploadedsignal is the name attribute of the input element
+        
         # check format type and choose the correct way to read it (dont forget to download the required library to read such files)
         # sample that data after reading it
         # transform that data into json (jsonify it )

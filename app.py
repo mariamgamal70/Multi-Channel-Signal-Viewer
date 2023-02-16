365c95aab0cf8ef45d9e8c4427abe655445e5200
mariamgamal70 <mariamgamal70@gmail.com>
2023-02-16 14:03:17 +0200

    adjusted file structure for flask

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request
 # Handle the HTTP request
 app = Flask(__name__)
+
 @app.route('/',methods=["POST","GET"])
 def getpath():
     if request.method == 'POST':
         filePath = request.form['filePath']
+        print(filePath)
     return render_template("/index.html")    
 if __name__ == '__main__':
     app.run(debug=True)
